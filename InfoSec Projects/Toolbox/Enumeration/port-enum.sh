e8486bd1069e880e0afe0419bf87c21c76eef1a2
Nexxsys <Nexxsys@users.noreply.github.com>
2024-07-23 19:31:40 -0600

    Create port-enum.sh

---
+++ InfoSec Projects/Toolbox/Enumeration/port-enum.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# port enumeration using nmap
+target="$1"
+ports=$(nmap -p- --min-rate 1000 "$target" | grep "^ *[0-9]" | grep "open" | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//')
+
+echo "Running second nmap scan with open ports: $ports"
+
+nmap -p "$ports" -sC -sV -A "$target" | tee port-enum.txt
